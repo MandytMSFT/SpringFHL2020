9a3fdfc2a438337a8c1db8da97459c9ac1e7a86a
Miaofei Wang <Miaofei.Wang@microsoft.com>
2020-04-01 12:52:01 +0800

    Split first chart and dynamic chart to 2 functions

--- DynamicChartBuilder_Yeo/src/taskpane/taskpane.js
+++ DynamicChartBuilder_Yeo/src/taskpane/taskpane.js
@@ -6,8 +6,6 @@
 /* global console, document, Excel, Office */
 
 const chartWidth = 500, chartHeight = 400;
-//const dataSheetName = "Data";
-// const dataTableName = "Table4";
 const countryColumnName = "Countries";
 const tempColumnName = "TempColumn";
 const colorList = ["#afc97a","#cd7371","#729aca","#b65708","#276a7c","#4d3b62","#5f7530","#772c2a","#2c4d75","#f79646","#4bacc6","#8064a2","#9bbb59","#c0504d","#4f81bd"];
@@ -16,12 +14,11 @@ const chartName = "DynamicChart";
 
 let logResult = document.getElementById("consoleText");
 let columnCount = 0;
+let activeTableId;
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Excel) {
-    //document.getElementById("sideload-msg").style.display = "none";
-    //document.getElementById("app-body").style.display = "flex";
-    document.getElementById("createStaticChart").onclick = CreateFirstChart;
+    document.getElementById("createFirstChart").onclick = CreateFirstChart;
     document.getElementById("createDynamicChart").onclick = CreateDynamicChart;
   }
 });
@@ -38,7 +35,8 @@ export async function CreateFirstChart() {
       // Get active table
       let dataTable = dataTables.items[0];
       let dataSheet = context.workbook.worksheets.getActiveWorksheet();
-      let table = dataSheet.tables.getItem(dataTable.id);
+      activeTableId = dataTable.id;
+      let table = dataSheet.tables.getItem(activeTableId);
 
       table.load("columns");
       await context.sync();
@@ -57,11 +55,19 @@ export async function CreateFirstChart() {
       let countryRange = columns.getItem(countryColumnName).getDataBodyRange();
 
       // Create Chart
-      tempRange.copyFrom(columns.getItemAt(1).getDataBodyRange());
+      let dataColumn = columns.getItemAt(1); // Use the first data column as starting chart data
+      tempRange.copyFrom(dataColumn.getDataBodyRange());
+      table.sort.apply([{ key: columnCount, ascending: true }], true);
       let chart = dataSheet.charts.add(Excel.ChartType.barClustered, tempColumn.getRange());
       chart.set({ name: chartName, height: chartHeight, width: chartWidth });
-      chart.title.format.font.set({size: fontSize});
+      let headerRange = dataColumn.getHeaderRowRange();
+      headerRange.load("text");
       await context.sync();
+
+      // Set chart tile and style
+      chart.title.text = headerRange.text[0][0];
+      chart.title.format.font.set({size: fontSize});
+      chart.legend.set({ visible: false });
 
       // Set category names
       let categoryAxis = chart.axes.getItem(Excel.ChartAxisType.category);
@@ -72,7 +78,9 @@ export async function CreateFirstChart() {
       series.dataLabels.showCategoryName = true;
       series.points.load();
       await context.sync();
-      writeLog(series.points.count);
+      //writeLog(series.points.count);
+
+      // Set data points color
       for (let i = 0; i < series.points.count; i++) {
           series.points.getItemAt(i).format.fill.setSolidColor(colorList[i % colorList.length]);
       }
@@ -88,51 +96,21 @@ export async function CreateFirstChart() {
 export async function CreateDynamicChart() {
   try {
     await Excel.run(async context => {
-      const dataSheetName = document.getElementById("DataSheetName").value;
-      const dataSheet = context.workbook.worksheets.getItem(dataSheetName);
-
-      const dataTableName = document.getElementById("TableName").value;
-      let table = dataSheet.tables.getItem(dataTableName);
+      let dataSheet = context.workbook.worksheets.getActiveWorksheet();
+      let table = dataSheet.tables.getItem(activeTableId);
 
       table.load("columns");
       await context.sync();
       let columns = table.columns;
       let tempColumn = columns.getItemOrNullObject(tempColumnName);
       let tempRange = tempColumn.getDataBodyRange();
-      //let countryRange = columns.getItem(countryColumn).getDataBodyRange();
-      tempRange.load("address");
-      //countryRange.load("address");
+      //tempRange.load("address");
       await context.sync();
-      //console.log("column count: " + columnCount);
-      writeLog("temp range:" + tempRange.address);
-      //console.log("country range: " + countryRange.address);
-
-      // let charts = dataSheet.charts;
-      // let chart = charts.add(Excel.ChartType.barClustered, tempColumn.getRange());
-      // chart.height = chartHeight;
-      // chart.width = chartWidth;
-      // chart.title.format.font.set({size: fontSize});
-      // await context.sync();
-
-      // let categoryAxis = chart.axes.getItem(Excel.ChartAxisType.category);
-      // categoryAxis.setCategoryNames(countryRange);
-      // let series = chart.series.getItemAt(0);
-      // series.hasDataLabels = true;
-      // series.gapWidth = 30;
-      // series.dataLabels.showCategoryName = true;
-      // series.points.load();
-      // await context.sync();
-      // console.log(countryRange);
-      // console.log(series.points.count);
-      // for (let i = 0; i < series.points.count; i++) {
-      //     series.points.getItemAt(i).format.fill.setSolidColor(colorList[i % colorList.length]);
-      // }
-      // await context.sync();
+      //writeLog("temp range:" + tempRange.address);
 
       let chart = dataSheet.charts.getItem(chartName);
+      let interval = document.getElementById("ChartSpeed").value;
 
-      let interval = document.getElementById("ChartSpeed").value
-      //for (let i = columnCount - 1; i > 0; i--) {
       for (let i = 1; i < columnCount; i++) {
           let dataRange = columns.getItemAt(i).getDataBodyRange();
           let titleRange = columns.getItemAt(i).getHeaderRowRange();
@@ -151,39 +129,6 @@ export async function CreateDynamicChart() {
   } catch (error) {
     writeLog(error);
   }
-}
-
-// Pivot table cannot sort and data point color doesn't keep the same when change data hierachy.
-async function testPivotTable() {
-  await Excel.run(async (context) => {
-      let book = context.workbook;
-      let sheet = context.workbook.worksheets.getItem(dataSheetName);
-      let pivotSheet = book.worksheets.getItem(pivotSheetName);
-      // Clear pivot sheet
-      let deprecatedRange = pivotSheet.getRange(null);
-      deprecatedRange.clear();
-      await context.sync();
-      let table = sheet.tables.getItemAt(0);
-      let pivotTable = book.pivotTables.add("PivotTable1", table.getRange(), pivotSheet.getRange("A1"));
-      //await context.sync();
-      pivotTable.rowHierarchies.add(pivotTable.hierarchies.getItem(countryColumn));
-      pivotTable.dataHierarchies.add(pivotTable.hierarchies.getItem("2020-03-25"));
-      await context.sync();
-  });
-}
-async function testTable() {
-  await Excel.run(async (context) => {
-      let book = context.workbook;
-      let sheet = context.workbook.worksheets.getItem(dataSheetName);
-      let table = sheet.tables.getItemAt(0);
-      table.load("columns");
-      await context.sync();
-      let columns = table.columns;
-      let columnCount = columns.count;
-      writeLog("count: " + columnCount);
-      table.sort.apply([{ key: 1, ascending: true }], true);
-      await context.sync();
-  });
 }
 
 function sleep(sleepTime) {
