8933cd7e4027b3237f0e086b14633f0e09465ed6
Miaofei Wang <Miaofei.Wang@microsoft.com>
2020-04-01 12:09:14 +0800

    temporary commit for merge

--- DynamicChartBuilder_Yeo/src/taskpane/taskpane.js
+++ DynamicChartBuilder_Yeo/src/taskpane/taskpane.js
@@ -8,18 +8,77 @@
 const chartWidth = 500, chartHeight = 400;
 //const dataSheetName = "Data";
 // const dataTableName = "Table4";
-const countryColumn = "Countries";
+const countryColumnName = "Countries";
 const tempColumnName = "TempColumn";
 const colorList = ["#afc97a","#cd7371","#729aca","#b65708","#276a7c","#4d3b62","#5f7530","#772c2a","#2c4d75","#f79646","#4bacc6","#8064a2","#9bbb59","#c0504d","#4f81bd"];
 const fontSize = 20;
+const chartName = "DynamicChart";
+
+let logResult = document.getElementById("consoleText");
+let columnCount = 0;
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Excel) {
     //document.getElementById("sideload-msg").style.display = "none";
     //document.getElementById("app-body").style.display = "flex";
+    document.getElementById("createStaticChart").onclick = CreateFirstChart;
     document.getElementById("createDynamicChart").onclick = CreateDynamicChart;
   }
 });
+
+export async function CreateFirstChart() {
+  try {
+    await Excel.run(async context => {
+      const dataSheetName = document.getElementById("DataSheetName").value;
+      const dataSheet = context.workbook.worksheets.getItem(dataSheetName);
+
+      const dataTableName = document.getElementById("TableName").value;
+      let table = dataSheet.tables.getItem(dataTableName);
+
+      table.load("columns");
+      await context.sync();
+      let columns = table.columns;
+      columnCount = columns.count;
+      let tempColumn = columns.getItemOrNullObject(tempColumnName);
+      await context.sync();
+      if (tempColumn.isNullObject) {
+          tempColumn = columns.add(null, null, "TempColumn");
+      }
+      else {
+          columnCount -= 1;
+      }
+
+      let tempRange = tempColumn.getDataBodyRange();
+      let countryRange = columns.getItem(countryColumnName).getDataBodyRange();
+
+      // Create Chart
+      tempRange.copyFrom(columns.getItemAt(1).getDataBodyRange());
+      let chart = dataSheet.charts.add(Excel.ChartType.barClustered, tempColumn.getRange());
+      chart.set({ name: chartName, height: chartHeight, width: chartWidth });
+      chart.title.format.font.set({size: fontSize});
+      await context.sync();
+
+      // Set category names
+      let categoryAxis = chart.axes.getItem(Excel.ChartAxisType.category);
+      categoryAxis.setCategoryNames(countryRange);
+      categoryAxis.visible = false;
+      let series = chart.series.getItemAt(0);
+      series.set({ hasDataLabels: true, gapWidth: 30 });
+      series.dataLabels.showCategoryName = true;
+      series.points.load();
+      await context.sync();
+      writeLog(series.points.count);
+      for (let i = 0; i < series.points.count; i++) {
+          series.points.getItemAt(i).format.fill.setSolidColor(colorList[i % colorList.length]);
+      }
+      await context.sync();
+    });
+  } 
+  catch (error) {
+    writeLog(error);
+  }
+
+}
 
 export async function CreateDynamicChart() {
   try {
@@ -33,44 +92,39 @@ export async function CreateDynamicChart() {
       table.load("columns");
       await context.sync();
       let columns = table.columns;
-      let columnCount = columns.count;
       let tempColumn = columns.getItemOrNullObject(tempColumnName);
-      await context.sync();
-      if (tempColumn.isNullObject) {
-          tempColumn = columns.add(null, null, "TempColumn");
-      }
-      else {
-          columnCount -= 1;
-      }
       let tempRange = tempColumn.getDataBodyRange();
-      let countryRange = columns.getItem(countryColumn).getDataBodyRange();
+      //let countryRange = columns.getItem(countryColumn).getDataBodyRange();
       tempRange.load("address");
-      countryRange.load("address");
+      //countryRange.load("address");
       await context.sync();
-      console.log("column count: " + columnCount);
-      console.log("temp range:" + tempRange.address);
-      console.log("country range: " + countryRange.address);
-      let charts = dataSheet.charts;
-      let chart = charts.add(Excel.ChartType.barClustered, tempColumn.getRange());
-      chart.height = chartHeight;
-      chart.width = chartWidth;
-      chart.title.format.font.set({size: fontSize});
-      await context.sync();
+      //console.log("column count: " + columnCount);
+      writeLog("temp range:" + tempRange.address);
+      //console.log("country range: " + countryRange.address);
 
-      let categoryAxis = chart.axes.getItem(Excel.ChartAxisType.category);
-      categoryAxis.setCategoryNames(countryRange);
-      let series = chart.series.getItemAt(0);
-      series.hasDataLabels = true;
-      series.gapWidth = 30;
-      series.dataLabels.showCategoryName = true;
-      series.points.load();
-      await context.sync();
-      console.log(countryRange);
-      console.log(series.points.count);
-      for (let i = 0; i < series.points.count; i++) {
-          series.points.getItemAt(i).format.fill.setSolidColor(colorList[i % colorList.length]);
-      }
-      await context.sync();
+      // let charts = dataSheet.charts;
+      // let chart = charts.add(Excel.ChartType.barClustered, tempColumn.getRange());
+      // chart.height = chartHeight;
+      // chart.width = chartWidth;
+      // chart.title.format.font.set({size: fontSize});
+      // await context.sync();
+
+      // let categoryAxis = chart.axes.getItem(Excel.ChartAxisType.category);
+      // categoryAxis.setCategoryNames(countryRange);
+      // let series = chart.series.getItemAt(0);
+      // series.hasDataLabels = true;
+      // series.gapWidth = 30;
+      // series.dataLabels.showCategoryName = true;
+      // series.points.load();
+      // await context.sync();
+      // console.log(countryRange);
+      // console.log(series.points.count);
+      // for (let i = 0; i < series.points.count; i++) {
+      //     series.points.getItemAt(i).format.fill.setSolidColor(colorList[i % colorList.length]);
+      // }
+      // await context.sync();
+
+      let chart = dataSheet.charts.getItem(chartName);
 
       for (let i = 1; i < columnCount; i++) {
           let dataRange = columns.getItemAt(i).getDataBodyRange();
@@ -87,7 +141,7 @@ export async function CreateDynamicChart() {
       await context.sync();
     });
   } catch (error) {
-    console.error(error);
+    writeLog(error);
   }
 }
 
@@ -118,11 +172,12 @@ async function testTable() {
       await context.sync();
       let columns = table.columns;
       let columnCount = columns.count;
-      console.log("count: " + columnCount);
+      writeLog("count: " + columnCount);
       table.sort.apply([{ key: 1, ascending: true }], true);
       await context.sync();
   });
 }
+
 function sleep(sleepTime) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
@@ -131,6 +186,11 @@ function sleep(sleepTime) {
       }
   }
 }
+
+function writeLog(log) {
+  logResult.innerText = logResult.innerText + '\n' + log;
+}
+
 /** Default helper for invoking an action and handling errors. */
 async function tryCatch(callback) {
   try {
